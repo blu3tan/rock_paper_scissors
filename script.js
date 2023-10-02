c69fb05b699df09c45f29440bb5f0e22dcfef029
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-02 17:35:01 +0200

    Add function to play 5 rounds and keep scores
    Added 2 functions, one to loop 5 times a single round.
    The second to keep the scores and print the outcome of the game
    on the console.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// global variables
+
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+
+// generate the random computer choice
 
 function getComputerChoice() {
     let randomChoice = Math.floor (Math.random() * (3))
@@ -29,6 +36,8 @@ function getPlayerChoice() {
   return input;
 }
 
+// validate the player input to avoid everything other than RPS
+
 function validateInput(choice) {
     if ((choice === "rock" ||
          choice === "paper" ||
@@ -40,17 +49,54 @@ function validateInput(choice) {
     }
 }
 
-function playSingleRound (playerHand, computerHand) {
+// play one round of RPS
+
+function playSingleRound () {
+    const playerHand = getPlayerChoice();
+    const computerHand = getComputerChoice();
+
     while (playerHand === computerHand) {
+        ties += 1;
         return `Tie! you both choose ${playerHand}`;
     }
     if   ((playerHand === "rock" && computerHand === "scissors" ||
          playerHand === "paper" && computerHand === "rock" ||
          playerHand === "scissors" && computerHand === "paper"))
          {
+            playerScore += 1;
             return `You won! ${playerHand} beats ${computerHand}`;
          }
     else {
+            computerScore += 1;
             return `You loose! ${computerHand} beats ${playerHand}`;
     }
 }
+
+// play a game of 5 rounds of RPS
+
+function gameOf5 () {
+    for (let i = 0; i <=4; i++) {
+        playSingleRound();
+    }
+}
+
+// Keep track of scores and print on the console the outcome
+
+function scoreRegister() {
+    console.log(`${ties} rounds ended in a tie`)
+    console.log(`You won ${playerScore} rounds`);
+    console.log(`I won ${computerScore} rounds`);
+    
+    if (playerScore === computerScore) {
+        console.log("It was a faire game, we are even.")
+    }
+    else if (playerScore > computerScore) {
+        console.log("You Won the match!? I am speachless...")
+    }
+    else {
+        console.log("You Loose the the game! Very predictable outcome...")
+    }
+}
+
+gameOf5();
+scoreRegister();
